ae51502aa260e0d70c13b045ea5821c12ae60c97
Edwin Fernando <ef322@ic.ac.uk>
2024-11-05 21:59:09 +0000

    feat: print output from subprocess and parse it
    The reciever type need not necessarily be primitive
    Also the dumpKotlinLike pretty printer doesn't actaully produce compiling code for most constructs

--- kotlin-ir-plugin/src/main/kotlin/com/constant/eval/ConstSubsTransformer.kt
+++ kotlin-ir-plugin/src/main/kotlin/com/constant/eval/ConstSubsTransformer.kt
@@ -10,17 +10,18 @@ import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
 import org.jetbrains.kotlin.ir.types.isPrimitiveType
 import org.jetbrains.kotlin.ir.util.dumpKotlinLike
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformer
+import org.jetbrains.kotlin.util.removeSuffixIfPresent
 import java.io.File
 import java.io.IOException
-import kotlin.io.path.Path
-import kotlin.io.path.name
-import kotlin.math.exp
-
+import kotlin.io.path.createTempDirectory
 
 class ConstSubsTransformer(private val pluginContext: IrPluginContext): IrElementTransformer<Boolean> {
   private var evalFunCorrect: Boolean = true // only meaningful when inside an eval function or just came out of one
-  private val workingDir = Path("/homes/ef322/temp")
+  private val workingDir = createTempDirectory()
 
+  // Solely for the purpose of printing the transformed IR as a side effect. This was just for debugging,
+  // but I ran out of time to figure to properly look into `com.tschuchort.compiletesting` which is library used for testing
+  // So I just print the transformed IR when running the test to show it works
   override fun visitModuleFragment(declaration: IrModuleFragment, data: Boolean): IrModuleFragment {
     val foo = super.visitModuleFragment(declaration, data)
     println(declaration.dumpKotlinLike())
@@ -28,12 +29,14 @@ class ConstSubsTransformer(private val pluginContext: IrPluginContext): IrElemen
   }
 
   private fun isPrimitives(expression: IrCall): Boolean {
-    val callerType = expression.dispatchReceiver?.type ?: expression.extensionReceiver?.type
-    return expression.type.isPrimitiveType()
-      // check if caller is a primitive
-      && callerType != null && callerType.isPrimitiveType()
-      // no generics are allowed
-      && expression.typeArgumentsCount > 0 &&
+    // val callerType = expression.dispatchReceiver?.type ?: expression.extensionReceiver?.type
+    // (callerType == null || callerType.isPrimitiveType() || callerType.isSimpleType)
+    // the above condition was one of the conjuncts before. For some reason callerType.isSimpleType throws
+    // an IllegalArgument Exception for functions of type `simpleTypeImpl` such as `toInt`
+    // as a consequence have user defined classes as the receiver type will be erroneously allowed by the checker
+
+    // no generics are allowed
+    return expression.typeArgumentsCount == 0 &&
       // whether arguments are primitive
       // disallow varargs, and default values in eval functions since the type is not known at this point
       expression.valueArguments.all { it?.type?.isPrimitiveType() ?: false }
@@ -62,12 +65,15 @@ class ConstSubsTransformer(private val pluginContext: IrPluginContext): IrElemen
 
   private fun evaluateUsingKotlinc(irCall: IrCall, irFunDef: IrFunction): IrElement {
     val fileName = "exec.kts"
-    val evalScript = File(workingDir.name, fileName)
+    val evalScript = File(workingDir.toString(), fileName)
     evalScript.createNewFile()
-    evalScript.writeText(irCall.dumpKotlinLike())
-    evalScript.appendText(irFunDef.dumpKotlinLike())
+
+    // wrap the call with a print, so the output of the subprocess can be retrieved from subprocess
+    evalScript.writeText("println(${irCall.dumpKotlinLike()})\n${irFunDef.dumpKotlinLike()}")
     val output = "kotlinc -script $fileName".runCommand(workingDir.toFile()) ?: return irCall
     return when (irCall.type) {
+      // Ideally this when statement is replaced with some reflection / other type level magic converting
+      // from the companion objects defined in the sealed class `IrConst` to the corresponding `to*()` function (eg. `toInt()`)
       pluginContext.irBuiltIns.intType -> IrConstImpl.int(0, 0, irCall.type, output.toInt())
       pluginContext.irBuiltIns.stringType -> IrConstImpl.string(0, 0, irCall.type, output)
       pluginContext.irBuiltIns.booleanType -> IrConstImpl.boolean(0, 0, irCall.type, output.toBoolean())
@@ -76,6 +82,7 @@ class ConstSubsTransformer(private val pluginContext: IrPluginContext): IrElemen
     }
   }
 
+  // from https://stackoverflow.com/a/41495542
   private fun String.runCommand(workingDir: File): String? {
     try {
         val parts = this.split("\\s".toRegex())
@@ -85,11 +92,15 @@ class ConstSubsTransformer(private val pluginContext: IrPluginContext): IrElemen
                 .redirectError(ProcessBuilder.Redirect.PIPE)
                 .start()
 
-        return proc.inputStream.bufferedReader().readText()
+      proc.waitFor()
+      return if (proc.exitValue() != 0) {
+        null
+      } else {
+        proc.inputStream.bufferedReader().readText().removeSuffixIfPresent("\n")
+      }
     } catch(e: IOException) {
         e.printStackTrace()
-      assert(false)
-        return "fail"
+      return null
     }
-}
+  }
 }
--- kotlin-ir-plugin/src/test/kotlin/com/constant/eval/IrPluginTest.kt
+++ kotlin-ir-plugin/src/test/kotlin/com/constant/eval/IrPluginTest.kt
@@ -22,13 +22,17 @@ import com.tschuchort.compiletesting.JvmCompilationResult
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
-import kotlin.test.assertEquals
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class IrPluginTest {
+
   @Test
-  fun `IR plugin success`() {
+  fun nestedEval() {
+    println("Here is the transformed IR")
+    // Printing happens as side effect. Ideally I look into the compiler-test-library api to see how get the IR here in the test
+    // rather than as part of implementation
     val result = compile(
       sourceFile = SourceFile.kotlin(
         "main.kt", """
@@ -36,28 +40,54 @@ fun main() {
   println(evalDebug())
 }
 
-fun evalDebug() = "Hello, World!"
+fun evalDebug(): Int {
+    val foo = evalId("5".toInt())
+    return foo
+}
+fun evalId(x: Int) = x
 """
       )
     )
     assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
   }
+
+  @Test
+  fun simpleFunction() {
+    val result = compile(
+      sourceFile = SourceFile.kotlin(
+        "main.kt", """
+fun main() {
+  println(evalDebug())
 }
 
-fun compile(
+fun evalDebug(): Int {
+    var bar = 6
+    bar += 10
+    return bar
+}
+"""
+      )
+    )
+
+    assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
+  }
+
+
+  fun compile(
     sourceFiles: List<SourceFile>,
     plugin: CompilerPluginRegistrar = ConstEvalCompilerRegistrar(),
-): JvmCompilationResult {
-  return KotlinCompilation().apply {
-    sources = sourceFiles
-    compilerPluginRegistrars = listOf(plugin)
-    inheritClassPath = true
-  }.compile()
-}
+  ): JvmCompilationResult {
+    return KotlinCompilation().apply {
+      sources = sourceFiles
+      compilerPluginRegistrars = listOf(plugin)
+      inheritClassPath = true
+    }.compile()
+  }
 
-fun compile(
+  fun compile(
     sourceFile: SourceFile,
     plugin: CompilerPluginRegistrar = ConstEvalCompilerRegistrar(),
-): JvmCompilationResult {
-  return compile(listOf(sourceFile), plugin)
+  ): JvmCompilationResult {
+    return compile(listOf(sourceFile), plugin)
+  }
 }
